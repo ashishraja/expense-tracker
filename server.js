6e31ac79ea469e01c071158a7fba65b64ed94a31
Jyoti Santani <jyotisantani@Jyotis-MacBook-Air.local>
2024-05-27 18:05:12 +0530

    new-feature

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const dotenv  = require("dotenv");
+
+dotenv.config({
+  path:"Config/config.env",
+});
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -34,7 +40,7 @@ const expenseSchema = mongoose.Schema({
 
 const userExpense = mongoose.model("UserExpense", expenseSchema);
 
-app.post("/", (req, res) => {
+app.post("/", async(req, res) => {
     var amount = Number(req.body.amount);
     var category = req.body.category;
     var item = req.body.item;
@@ -54,22 +60,60 @@ app.post("/", (req, res) => {
         "date": date,
         "totalAmount": totalAmount
     }
-    
-    const expense = userExpense.create(expenseData);
+
+    await userExpense.create(expenseData);
     res.redirect("/expense");
 });
 
 app.get("/", function (req, res) {
-    userExpense.find().then(function (foundItems) {
-        if (foundItems.length !== 0) {
-            res.render("table", { listTitle: "All Expenses and Income", totalAmount: totalAmount, newListItems: foundItems });
-        } else {
-            res.redirect("/");
-        }
-    })
-    res.render("index.html");
+    res.render("index");
 });
 
+app.get("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+    const expense = await userExpense.findById(id);
+    res.render("edit" ,{ editTitle : "Update" , newListItem:expense });
+});
+
+app.post("/edit/:id", async (req, res) => {
+    const updatedAmount = Number(req.body.updatedAmount);
+    const updatedItem = req.body.updatedItem;
+    const updatedCategory = req.body.updatedCategory;
+    const updatedDate = req.body.updatedDate;
+    const expense = await userExpense.findById(req.params.id);
+
+    if (updatedAmount) {
+
+        if (updatedCategory === "Income") {
+            if (updatedCategory === "Income" && updatedCategory === expense.category) {
+                totalAmount = totalAmount - expense.amount + updatedAmount;
+            } else {
+                totalAmount = totalAmount + expense.amount + updatedAmount;
+            }
+        }
+
+        if (updatedCategory === "Expense") {
+            if (updatedCategory === "Expense" && updatedCategory === expense.category) {
+                totalAmount = totalAmount + expense.amount - updatedAmount;
+            } else {
+                totalAmount = totalAmount - expense.amount - updatedAmount;
+            }
+        }
+        expense.amount = updatedAmount;
+    }
+    if (updatedCategory) {
+        expense.category = updatedCategory;
+    }
+    if (updatedItem) {
+        expense.item = updatedItem;
+    }
+    if (updatedDate) {
+        expense.date = updatedDate;
+    }
+    await expense.save();
+    res.redirect("/expense");
+
+});
 
 
 app.get("/expense", function (req, res) {
@@ -86,8 +130,8 @@ app.post("/delete", async function (req, res) {
     const checkedItemId = req.body.delete;
     const expense = await userExpense.findById(checkedItemId);
     if (expense.category === "Expense") {
-            totalAmount += expense.amount;
-    }else{
+        totalAmount += expense.amount;
+    } else {
         totalAmount -= expense.amount;
     }
     await expense.deleteOne()
@@ -95,6 +139,6 @@ app.post("/delete", async function (req, res) {
 });
 
 
-app.listen(process.env.PORT , () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on port : ${process.env.PORT}`);
 });
